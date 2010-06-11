d01924b9a3c026ebfcdb06d65e558eed15b027b2
William Roberts <williamr@symbian.org>
2010-06-11 17:54:34 +0100

    Add definition of _FOFF_DYNAMIC(c,f) as an alternative to _FOFF(c,f) where f cannot be computed at compile time
    --HG--
    branch : GCC_SURGE

--- kernel/eka/include/e32def.h
+++ kernel/eka/include/e32def.h
@@ -443,8 +443,9 @@ the link object from the start of a list element.
 @see TDblQue
 */
 #ifndef _FOFF
+#define _FOFF_DYNAMIC(c,f)	(((TInt)&(((c *)0x1000)->f))-0x1000)		// can be a runtime calculation if f is not sufficiently constant
 #if __GNUC__ < 4
-#define _FOFF(c,f)			(((TInt)&(((c *)0x1000)->f))-0x1000)
+#define _FOFF(c,f)			_FOFF_DYNAMIC(c,f)
 #else
 #define _FOFF(c,f)			(__builtin_offsetof(c,f))
 #endif
