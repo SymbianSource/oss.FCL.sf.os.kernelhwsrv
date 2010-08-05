ff8c2a39a6d9eda720c703bbf6f44ebf39898717
Mike Kinghan <mikek@symbian.org>
2010-08-05 10:36:25 +0100

    Workaround for bug 2976 -  [GCCE] Overlapping naked function bodies break exception-handling. We have no fix that will compile the problem file kernel/eka/common/arm/carray.cia, so instead we just configure the c++ implementation of CArray instead of the assembly coded one.
    --HG--
    branch : GCC_SURGE

--- kernel/eka/include/cpudefs.h
+++ kernel/eka/include/cpudefs.h
@@ -247,7 +247,9 @@
 	#define __COBJECT_MACHINE_CODED__
 	#define __CACTIVESCHEDULER_MACHINE_CODED__
 	#define __CSERVER_MACHINE_CODED__
-//	#define __ARRAY_MACHINE_CODED__ IMK working around bug #2976
+#ifndef __GCCE__ // IMK working around bug #2976 for GCCE
+	#define __ARRAY_MACHINE_CODED__
+#endif 
 	#define __HUFFMAN_MACHINE_CODED__
 #if defined(__MARM_ARM4__) || defined(__MARM_ARMI__) || defined(__MARM_THUMB__) || defined(__MARM_ARMV4__) || defined(__MARM_ARMV5__)
 	#define __DES16_MACHINE_CODED_HWORD__
